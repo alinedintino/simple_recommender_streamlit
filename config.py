9e3c15d86d71e55f132d84cc79f8cb06b857651b
Aline D'Intino <64786515+alinedintino@users.noreply.github.com>
2022-04-22 09:18:29 +0200

    add files

---
+++ config.py
@@ -0,0 +1,17 @@
+
+score_based_cfg = dict(
+    title="Best rated movies",
+    description="""
+    Simple recommenders: offer generalized recommendations based on movie popularity and/or genre. The basic idea behind this system is that more popular and 
+    critically acclaimed movies will have a higher probability of being liked by you. 
+
+    """
+)
+
+content_based_cfg = dict(
+    title="Because you watched...",
+    description="""
+    Content-based recommenders: suggest similar items based on a movie you have watched. The general idea 
+    behind these recommender systems is that you probably also want to watch a movie similar to what you liked before.
+    """
+)
